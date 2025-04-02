ae660df01affe0404b1e48cafee7d20e764aa0b4
enkhjin9127 <ka0enkhjin@gmail.com>
2025-04-02 08:16:51 +0800

    hi

--- src/controllers/foods/create-food.js
+++ src/controllers/foods/create-food.js
@@ -2,13 +2,26 @@ import Food from "../../models/Food.js";
 
 const createFood = async (req, res) => {
   try {
-    const { name, price, category, description } = req.body;
+    const { name, price, category, description, image, ingredients } = req.body;
+
+    if (
+      !name ||
+      !price ||
+      !category ||
+      !image ||
+      !ingredients ||
+      !ingredients.length
+    ) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
 
     const newFood = new Food({
       name,
       price,
       category,
       description,
+      image,
+      ingredients,
     });
 
     await newFood.save();
